807ff80b61c4df7d4bab6fdb8cdd05aa4ba786de
avgolov <golov.andrey@hotmail.com>
2021-09-23 09:05:25 +0300

    Conv1d

--- src/models/CQT.py
+++ src/models/CQT.py
@@ -3,6 +3,8 @@ import timm
 import torch
 import torch.nn as nn
 from nnAudio.Spectrogram import CQT1992v2
+import torch.nn.functional as F
+
 
 class CustomModel_v1(nn.Module):
     def __init__(
@@ -257,3 +259,120 @@ class CustomModelWavegram(nn.Module):
         x = cqt(x).unsqueeze(1)
         x = nn.functional.interpolate(x, (self.h, self.w))
         return x
+
+    
+class GeM(nn.Module):
+    '''
+    Code modified from the 2d code in
+    https://amaarora.github.io/2020/08/30/gempool.html
+    '''
+
+    def __init__(self, kernel_size=8, p=3, eps=1e-6):
+        super(GeM, self).__init__()
+        self.p = nn.Parameter(torch.ones(1) * p)
+        self.kernel_size = kernel_size
+        self.eps = eps
+
+    def forward(self, x):
+        return self.gem(x, p=self.p, eps=self.eps)
+
+    def gem(self, x, p=3, eps=1e-6):
+        return F.avg_pool1d(x.clamp(min=eps).pow(p), self.kernel_size).pow(1. / p)
+
+    def __repr__(self):
+        return self.__class__.__name__ + \
+               '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + \
+               ', ' + 'eps=' + str(self.eps) + ')'
+
+    
+class CNN1d(nn.Module):
+    """1D convolutional neural network. Classifier of the gravitational waves.
+    Architecture from there https://journals.aps.org/prl/pdf/10.1103/PhysRevLett.120.141103
+    """
+
+    def __init__(self, debug=False):
+        super().__init__()
+        self.cnn1 = nn.Sequential(
+            nn.Conv1d(3, 64, kernel_size=64),
+            nn.BatchNorm1d(64),
+            nn.SiLU(),
+        )
+        self.cnn2 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=32),
+            GeM(kernel_size=8),
+            nn.BatchNorm1d(64),
+            nn.SiLU(),
+        )
+        self.cnn3 = nn.Sequential(
+            nn.Conv1d(64, 128, kernel_size=32),
+            nn.BatchNorm1d(128),
+            nn.SiLU(),
+        )
+        self.cnn4 = nn.Sequential(
+            nn.Conv1d(128, 128, kernel_size=16),
+            GeM(kernel_size=6),
+            nn.BatchNorm1d(128),
+            nn.SiLU(),
+        )
+        self.cnn5 = nn.Sequential(
+            nn.Conv1d(128, 256, kernel_size=16),
+            nn.BatchNorm1d(256),
+            nn.SiLU(),
+        )
+        self.cnn6 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=16),
+            GeM(kernel_size=4),
+            nn.BatchNorm1d(256),
+            nn.SiLU(),
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(256 * 11, 64),
+            nn.BatchNorm1d(64),
+            nn.Dropout(0.25),
+            nn.SiLU(),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(64+20, 64),
+            nn.BatchNorm1d(64),
+            nn.Dropout(0.25),
+            nn.SiLU(),
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(64, 1),
+        )
+        self.rnn = RNN()
+        self.debug = debug
+
+    def forward(self, x, pos=None):
+        x = self.cnn1(x)
+        x = self.cnn2(x)
+        x_rnn = self.rnn(x)
+        x = self.cnn3(x)
+        x = self.cnn4(x)
+        x = self.cnn5(x)
+        x = self.cnn6(x)
+        x = x.flatten(start_dim=1)
+        x = self.fc1(x)
+        x = torch.cat([x, x_rnn], dim=1)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+class RNN(nn.Module):
+    def __init__(self):
+         super(RNN, self).__init__()
+
+         self.rnn0 = nn.LSTM(input_size=500, hidden_size=10, batch_first=True, bidirectional=True)
+         self.fc = nn.Sequential(
+            nn.Linear(64 * 20, 20),
+            nn.BatchNorm1d(20),
+            nn.Dropout(0.25),
+            nn.SiLU(),
+        )
+
+    def forward(self, x):
+         r_out, (h_n, h_c) = self.rnn0(x, None)
+         r_out = torch.reshape(r_out, (-1, 64 * 20))
+         out = self.fc(r_out)
+         return out
